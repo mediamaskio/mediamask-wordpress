0330dd141f906a4ed816941a30e4febf59f175a4
Gregor Vostrak <gregorvostrak@Gregors-MacBook-Pro.local>
2022-09-17 03:20:59 +0200

    bump version

--- mediamask.php
+++ mediamask.php
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'MEDIAMASK_VERSION', '1.0.14' );
+define( 'MEDIAMASK_VERSION', '1.0.15' );
 
 /**
  * The code that runs during plugin activation.
